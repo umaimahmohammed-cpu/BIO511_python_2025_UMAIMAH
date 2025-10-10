e0f35f06b6be532f54db35a594ddba352b8c46a9
Umaimah Mohammed Hassen <umaimah@vera2.int.private>
2025-10-10 03:44:07 +0200

    no massege

--- practical_3.py
+++ practical_3.py
@@ -31,3 +31,32 @@ print (count)
 #    - Print the GLOBAL count again (notice the global didn’t change).
 print (count)
 
+
+4.2
+# Text summary  (uses text, summary)
+# Goal: classify characters with an if/elif/else chain and return a clear result.
+summary ={"digits" : 0, "letters" : 0, "other" : 0}
+# a) Define a function named summarize_text that takes one argument: s.
+def summarize_text (s):
+    summary ={"digits" : 0, "letters" : 0, "other" : 0} # b)
+    for suma in s:                               # c) Loop through each character in s:
+        if suma.isdigit():
+             summary ["digits"] += 1 #       - if the character is a digit, increase "digits"
+        elif suma.isalpha():
+            summary["letters"] += 1   #       - elif the character is a letter, increase "letters"
+        else:
+            summary ["other"]+= 1           #       - else increase "other"
+    return summary                 # d) Return the summary dictionary.______
+
+# e) Outside the function:
+#    - Print the GLOBAL summary.
+print (summary)
+#    - Call summarize_text(text) and print the returned dictionary.
+x= summarize_text("hi26??")
+print (x)
+#    - Print the GLOBAL summary again.
+print (summary)
+
+
+
+
